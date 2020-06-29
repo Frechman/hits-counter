b5af5d7ea6e2612083f94ec2bbe96d1c87f2cddf
sj <serj.g13@yandex.ru>
2020-06-29 13:40:25 +0300

    init

---
+++ src/main/java/ru/gavrilov/counter/Application.java
@@ -0,0 +1,13 @@
+package ru.gavrilov.counter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
